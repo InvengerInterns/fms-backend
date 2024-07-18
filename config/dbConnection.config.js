b636479403892cf7b422b23e2bb7736780131a63
prabhupradyumna <prabhupradyumna@gmail.com>
2024-07-18 10:48:32 +0530

    connection to the database set

--- config/dbConnection.config.js
+++ config/dbConnection.config.js
@@ -1,1 +1,13 @@
-console.log('Connect DB');
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,{
+        host : process.env.DB_HOST,
+        dialect : 'mysql'
+    }
+)
+
+export default sequelize;
