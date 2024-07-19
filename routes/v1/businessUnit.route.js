fe7e221765c0d27b91b3d9670e2e32ca2aeeb0d0
roy-lew <20cs127.riswan@sjec.ac.in>
2024-07-19 12:23:53 +0530

    Updated business unit module

--- routes/v1/businessUnit.route.js
+++ routes/v1/businessUnit.route.js
@@ -8,10 +8,13 @@ import {
 
 const router = express.Router();
 
-//Register Route
+//Register business Route
 router.post('/add-business-unit', createBusinessUnit);
+//update  business unit Route
 router.put('/update-business-unit/:id', updateBusinessUnit);
+//delete business unit Route
 router.delete('/delete-business-unit/:id', deleteBusinessUnit);
+//get business unit Route
 router.get('/get-business-unit', getBusinessUnits);
 
 export default router;
