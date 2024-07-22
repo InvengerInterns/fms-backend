45ccf164c0e70b32cfbf773793763298f213258f
nishanth-s03 <22ca060.nishanth@sjec.ac.in>
2024-07-22 12:44:22 +0530

    Fixed: User Verification bug

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -9,6 +9,8 @@ import {
   sendOtp,
   verifyOtp,
 } from '../../controllers/user.controller.js';
+import { protect } from '../../middlewares/auth.middleware.js';
+
 
 const router = express.Router();
 
@@ -19,14 +21,14 @@ router.put('/password-update', createPassword);
 //Login User
 router.post('/login-user', loginUser);
 //Get User Route
-router.get('/get-user/:employeeId', getUserByEmployeeId);
+router.get('/get-user/:employeeId',protect,getUserByEmployeeId);
 //Delete User Route
-router.put('/delete-user/:employeeId', deleteUserByEmployeeId);
+router.put('/delete-user/:employeeId',protect,deleteUserByEmployeeId);
 //Logout User
-router.post('/logout-user', logoutUser);
+router.post('/logout-user', protect, logoutUser);
 //Send OTP via Mail
-router.post('/send-otp', sendOtp);
+router.post('/send-otp', protect, sendOtp);
 //Verify OTP
-router.post('/verify-otp/:email', verifyOtp);
+router.post('/verify-otp/:email', protect, verifyOtp);
 
 export default router;
