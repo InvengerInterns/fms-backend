84f40182ff6406d51355d2fa0f2af99ad146e65a
Sathv111 <20ai016.sathvik@sjec.ac.in>
2024-09-18 15:25:15 +0530

    Added:Department controller and Routes

---
+++ routes/v1/department.route.js
@@ -0,0 +1,23 @@
+import {
+  createDepartment,
+  updateDepartmentDetails,
+} from '../../controllers/department.controller.js';
+import express from 'express';
+import { allowedTo, protect } from '../../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post(
+    '/add-department',
+    protect,
+    allowedTo('admin'),
+    createDepartment
+  );
+
+  //update ManagerId or Department name 
+router.put(
+    '/update-department/:deparmentId',
+    protect,
+    allowedTo('admin'),
+    updateDepartmentDetails
+  );
