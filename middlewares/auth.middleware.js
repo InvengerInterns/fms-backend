f45623a576f9a2b70f15820338fc86b04ee3b46d
nishanth-s03 <nishubangerasz@gmail.com>
2025-01-24 15:12:47 +0530

    Updated: Refresh token updating with Caching

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,18 +2,16 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { promisify } from 'util';
 import User from '../models/user.model.js';
-import { getPermissionsForUser } from '../utils/user.util.js';
+import { getPermissionsForUser, signToken } from '../utils/user.util.js';
 
 dotenv.config();
 
 // JWT Verifying Token with Expiry Check
 const protect = async (req, res, next) => {
   try {
-    const rolesToCheck = ['admin','user'];
-
-    let token;
-
-    if (req.cookies.access_token) token = req.cookies.access_token;
+    const rolesToCheck = ['admin', 'user'];
+    let token = req.cookies.access_token;
+    const refreshToken = req.cookies.refresh_token;
 
     if (!token) {
       return res.status(404).json({ message: 'User Not LoggedIn' });
@@ -28,13 +26,32 @@ const protect = async (req, res, next) => {
     // Check if the token is expired
     const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
     if (decoded.payload.exp < currentTime) {
-      return res.status(401).json({
-        message: 'Token Expired. Please Login Again.',
-        statusCode: 'TokenExpired',
+      if (!refreshToken) {
+        return res.status(401).json({
+          message: 'Token Expired. Please Login Again.',
+          statusCode: 'TokenExpired',
+        });
+      }
+
+      // Verify refresh token
+      const verifiedRefreshToken = await promisify(jwt.verify)(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET
+      );
+
+      // Generate new access token if refresh token is valid
+      const newToken = await signToken(verifiedRefreshToken.id);
+
+      // Set new access token in cookies
+      res.cookie('access_token', newToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
       });
+
+      token = newToken; // Update the token to use for subsequent operations
     }
 
-    // Verify the token
+    // Verify the (new or existing) access token
     const verifiedToken = await promisify(jwt.verify)(
       token,
       process.env.JWT_SECRET
